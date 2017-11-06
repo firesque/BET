866f74247534731499931083c984b6ef3eb8ffc1
firesque <evanberey@gmail.com>
2017-11-06 01:07:49 +0000

    added index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>BET</title>
+  </head>
+  <body>
+    <form action="validation.php">
+      <center>
+        <h1>Welcome to</br>
+        BETH EL TEMPLE</h1>
+      <img src="img/BET.jpg" alt="Shalom">
+      </center>
+      <table>
+         <tr>
+           <td>User Name:</td><td><input type="text" name="uname"></td>
+         </tr>
+         <tr>
+           <td>Password: </td><td><input type="password" name="pw"></td>
+         </tr>
+      </table>
+      <input type="submit" value="Submit">
+    </form>
+  </body>
+</html>
---
+++ validation.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>BET</title>
+  </head>
+  <body>
+      <center>
+        <h1>Thanks for signing into</br>
+        SULAM</h1>
+      </center>
+  </body>
+</html>
